52807d7317c329289ad30a369e630e0bc4b56752
Matheus Henrique Ferreira Brandão <matheus.brandao@stoque.com.br>
2024-02-15 11:01:25 -0300

    lambda-teste

--- lambda.tf
+++ lambda.tf
@@ -4,11 +4,11 @@ data "archive_file" "init" {
   output_path = "main.py.zip"
 }
 
-#resource "aws_lambda_function" "lambda_cria_db" {
-#  function_name = "${var.cliente}-lambda"
-#  role          = ""
-#  filename = "jujuba.zip"
-#  handler = "main.handler"
-#  runtime = "python3.12"
-#}
+resource "aws_lambda_function" "lambda_cria_db" {
+  function_name = "${var.cliente}-lambda"
+  role          = "${aws_iam_role.role.arn}"
+  filename      = "main.py.zip"
+  handler       = "main.handler"
+  runtime       = "python3.12"
+}
 
