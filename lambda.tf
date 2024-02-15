f6b93b19752edc93cb39a17420411ca993932b3a
Matheus Henrique Ferreira Brandão <matheus.brandao@stoque.com.br>
2024-02-15 14:18:36 -0300

    lambda-teste

--- lambda.tf
+++ lambda.tf
@@ -7,8 +7,8 @@ data "archive_file" "init" {
 resource "aws_lambda_function" "lambda_cria_db" {
   function_name = "${var.cliente}-lambda"
   role          = "${aws_iam_role.role.arn}"
-  filename      = "main.py.zip"
-  handler       = "main.handler"
+  filename      = "lambda_handler.py.zip"
+  handler       = "lambda_handler.lambda_handler"
   runtime       = "python3.12"
 }
 
--- script/main.py
+++
@@ -1,7 +0,0 @@
-import pyodbc
-
-
-conn = pyodbc.connect('DRIVER={ODBC driver 18 for SQL Server};SERVER=advancedsharedev.cicbnvbklhye.us-east-1.rds.amazonaws.com;PORT=1433;UID=admin;PWD=Pa55w0rd!(;Encrypt=no',autocommit=True)
-cursor = conn.cursor()
-cursor.execute("CREATE DATABASE testematheus")
-
