8709ab3f6f9bc6159a0084c0c47eb7138a726492
Matheus Henrique Ferreira Brandão <matheus.brandao@stoque.com.br>
2024-02-15 11:29:06 -0300

    lambda-teste

--- lambda.tf
+++ lambda.tf
@@ -1,10 +1,14 @@
-data "archive_file" "init" {
+provider "archive_file" "init" {
   type        = "zip"
   source_file = "script"
   output_path = "main.py.zip"
 }
 
 resource "aws_lambda_function" "lambda_cria_db" {
+  provisioner "file" {
+    source      = "script/"
+    destination = "/"
+  }
   function_name = "${var.cliente}-lambda"
   role          = "${aws_iam_role.role.arn}"
   filename      = "main.py.zip"
