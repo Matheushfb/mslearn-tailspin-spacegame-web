21e00fe1d023b0ec6b33feaf73f0c3d53ecbb5a3
Matheus Henrique Ferreira Brandão <matheus.brandao@stoque.com.br>
2024-02-23 09:51:13 -0300

    step para criação de layer lambda

--- lambda.tf
+++ lambda.tf
@@ -16,8 +16,8 @@ resource "null_resource" "build_lambda_layers" {
 
   provisioner "local-exec" {
     #working_dir = "${var.layer_path}"
-    #command     = "npm install --production && cd ../ && zip -9 -r --quiet ${var.layer_name}.zip *"
-    command     = "ls -lah && python3 -m venv venv_pyodbc  && source venv_pyodbc/bin/activate && mkdir python && python -m pip install pyodbc -t script && zip -r ${var.layer_name}.zip python"
+    #command     = " ls -lah && python3 -m venv venv_pyodbc  && source venv_pyodbc/bin/activate && mkdir python && python -m pip install pyodbc -t script && zip -r ${var.layer_name}.zip python"
+    command      = "sudo apt update, ls -lah"
   }
 }
 
